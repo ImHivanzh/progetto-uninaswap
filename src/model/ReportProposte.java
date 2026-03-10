519780c5c829838e12863515f52032af3404c81c
Sabatino <sabatinocuciniello90@gmail.com>
2026-03-10 16:31:26 +0100

    refactor: rimozione classi ed enum inutilizzate
    Rimossi componenti inutilizzati:
    - ReportProposteController (controller mai istanziato)
    - ReportProposteDialog (GUI mai aperta)
    - ReportProposte (model usato solo dal controller rimosso)
    - StatoOfferta (enum con zero riferimenti)
    - PropostaDAO.getReportProposte() (metodo chiamato solo dal controller rimosso)
    
    Verificato: compilazione riuscita

--- src/model/ReportProposte.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public record ReportProposte(
-        int totaleVendita, int accettateVendita,
-        int totaleScambio, int accettateScambio,
-        int totaleRegalo, int accettateRegalo,
-        double valoreMinimoVendita, double valoreMassimoVendita, double valoreMedioVendita) {
-}
